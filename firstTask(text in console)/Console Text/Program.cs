4f57e1d79e46a3cce513fd6cc30b858b979539d9
Nazar Yuriyovych Kleban <nykleban@gmail.com>
2025-10-09 15:32:59 +0300

    First Task(writeLine text)

---
+++ firstTask(text in console)/Console Text/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Console_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- --- --- --- --- --- --- --- --- --- --- --- ---");
+            Console.WriteLine("It's easy to win forgiveness for being wrong;");
+            Console.WriteLine("being right is what gets you into real trouble.");
+            Console.WriteLine("Bjarne Stroustrup");
+            Console.WriteLine("--- --- --- --- --- --- --- --- --- --- --- --- ---");
+        }
+    }
+}
