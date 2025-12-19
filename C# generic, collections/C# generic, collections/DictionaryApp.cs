5d04f10fbf5589b8b11c19d78b34597efe40dd73
Nazar Yuriyovych Kleban <nykleban@gmail.com>
2025-12-19 18:11:21 +0200

    C# generic, collections

---
+++ C# generic, collections/C# generic, collections/DictionaryApp.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C__generic__collections
+{
+    /*Завдання 2
+Створіть додаток "Англо-український словник". Необхідно зберігати таку інформацію:
+
+Слово англійською мовою;
+Варіанти перекладу українською.
+Для зберігання інформації використовуйте Dictionary<T>.
+
+Додаток має надавати таку функціональність:
+
+Додавання слова та варіантів перекладу;
+Видалення слова;
+Видалення варіанта перекладу;
+Зміна слова;
+Зміна варіанта перекладу;
+Пошук перекладу слова.*/
+    internal class DictionaryApp
+    {
+        private Dictionary<string, List<string>> dictionary;
+
+        public DictionaryApp()
+        {
+            dictionary = new Dictionary<string, List<string>>();
+        }
+
+        public void AddWord(string ENword, string UAword)
+        {
+            ENword = ENword.Trim().ToLower();
+            if (!dictionary.ContainsKey(ENword))
+            {
+                dictionary[ENword] = new List<string>();
+                Console.WriteLine($"Слово '{ENword}' додано до словника.");
+            }
+            string[] translations = UAword.Split(',');
+
+            foreach (string item in translations)
+            {
+                string cleanTranslation = item.Trim().ToLower();
+
+                if (!string.IsNullOrWhiteSpace(cleanTranslation))
+                {
+                    if (!dictionary[ENword].Contains(cleanTranslation))
+                    {
+                        dictionary[ENword].Add(cleanTranslation);
+                        Console.WriteLine($" + додано переклад: {cleanTranslation}");
+                    }
+                }
+            }
+        }
+
+        public void RemoveWord(string ENword)
+        {
+            if (dictionary.ContainsKey(ENword))
+            {
+                dictionary.Remove(ENword);
+                Console.WriteLine($"Слово {ENword} видалено.");
+            }
+            else
+            {
+                Console.WriteLine($"Cлова {ENword} немає в словнику.");
+            }
+        }
+
+        public void RemoveTranslate(string UAword)
+        {
+            bool found = false;
+            foreach (string key in dictionary.Keys)
+            {
+                if (dictionary[key].Remove(UAword))
+                {
+                    Console.WriteLine($"Переклад '{UAword}' видалено зі слова '{key}'.");
+                    found = true;
+                }
+            }
+            if (!found) Console.WriteLine($"Переклад '{UAword}' не знайдено.");
+        }
+
+        public void ChangeEnWord(string oldWord, string newWord)
+        {
+            if (dictionary.ContainsKey(oldWord))
+            {
+                if (dictionary.ContainsKey(newWord))
+                {
+                    Console.WriteLine($"Слово {newWord} вже існує!");
+                    return;
+                }
+                List<string> translations = dictionary[oldWord];
+                dictionary.Remove(oldWord);
+                dictionary.Add(newWord, translations);
+                Console.WriteLine($"Слово EN '{oldWord}' замінено на '{newWord}'.");
+            }
+            else
+            {
+                Console.WriteLine($"Слово '{oldWord}' не знайдено.");
+            }
+        }
+
+        public void ChangeUaWord(string oldWord, string newWord)
+        {
+            foreach (string key in dictionary.Keys)
+            {
+                List<string> translations = dictionary[key];
+                int index = translations.IndexOf(oldWord);
+                if (index != -1)
+                {
+                    translations[index] = newWord;
+                    Console.WriteLine($"У слові '{key}' переклад '{oldWord}' замінено на '{newWord}'.");
+                }
+            }
+        }
+
+        public void SearchTranslate(string ENword)
+        {
+            if (dictionary.ContainsKey(ENword))
+            {
+                Console.Write($"Переклади для слова {ENword}: ");
+                Console.WriteLine(string.Join(", ", dictionary[ENword]));
+            }
+            else
+            {
+                Console.WriteLine($"Слова {ENword} немає в словнику.");
+            }
+        }
+        public void PrintAll()
+        {
+            foreach (var word in dictionary)
+            {
+                Console.WriteLine($"{word.Key}: {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
---
+++ C# generic, collections/C# generic, collections/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System;
+using System.Xml.Serialization;
+
+namespace C__generic__collections
+{
+    internal class Program
+    {
+        /*Завдання 1
+
+
+Завдання 2
+Створіть додаток "Англо-український словник". Необхідно зберігати таку інформацію:
+
+Слово англійською мовою;
+Варіанти перекладу українською.
+Для зберігання інформації використовуйте Dictionary<T>.
+
+Додаток має надавати таку функціональність:
+
+Додавання слова та варіантів перекладу;
+Видалення слова;
+Видалення варіанта перекладу;
+Зміна слова;
+Зміна варіанта перекладу;
+Пошук перекладу слова.*/
+
+        // Створіть generic-версію методу Swap для обміну значеннями двох змінних.
+        static void Swap<T>(T a, T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.WriteLine("Завдання 1");
+            int a = 0;
+            int b = 10;
+            Console.WriteLine("a = "+ a + "; b = " + b);
+            Console.WriteLine("Після Swap():");
+            Swap<int>(a, b);
+            Console.WriteLine("a = " + a + "; b = " + b);
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Завдання 2");
+            DictionaryApp dictionaryApp = new DictionaryApp();
+            dictionaryApp.AddWord("apple", "яблуко");
+            dictionaryApp.AddWord("apple", "апельсин");
+            dictionaryApp.AddWord("run", "бігти, керувати, проганяти, запускати програму");
+            dictionaryApp.AddWord("automobile", "автомобіль, машинка");
+            dictionaryApp.RemoveTranslate("проганяти");
+            dictionaryApp.ChangeEnWord("automobile", "car");
+            dictionaryApp.ChangeUaWord("машинка", "авто");
+            dictionaryApp.AddWord("book", "книга");
+            dictionaryApp.SearchTranslate("apple");
+            Console.WriteLine("------ Весь словник ------");
+            dictionaryApp.PrintAll();
+            Console.WriteLine("--------------------------");
+
+        }
+    }
+}
